7c1d1defdbf804aa996fd2a29dcf73fcffa04666
NikJoshi800 <joshinikhil991@gmail.com>
2017-02-18 13:39:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,53 +9,16 @@ var articleOnet= {
     title: 'Article one | Nik Joshi',
     heading:'Article one',
     date: 'Feb 08. 2017',
-    content:`  <p>
+    content:`  
+        <p>
         This is the content of my Article one.         This is the content of my Article one.        This is the content of my Article one.
             </p>
             <p>
           This is the content of my Article one.        This is the content of my Article one.        This is the content of my Article one.
-            </p>'
+            </p>`
  
 };
-function createTemplet(data){
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-var htmlTemplet='
-<html>
-    <head>
-        <title>
-           ${title}
-            </title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link href="/ui/style.css" rel="stylesheet" />
-          
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href='/'>Home</a>
-        </div>
-        <hr/>
-        <h3>
-        ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-        ${content}
-        </div>
-        </div>
-    </body>
-    
-</html>
 
-';
-return htmlTemplets;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
