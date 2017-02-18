dc858c0b9ad897e1230da4e9d6d46cdc4519049c
NikJoshi800 <joshinikhil991@gmail.com>
2017-02-18 13:35:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ var articleOnet= {
     title: 'Article one | Nik Joshi',
     heading:'Article one',
     date: 'Feb 08. 2017',
-    content:    
+    content:`  <p>
+        This is the content of my Article one.         This is the content of my Article one.        This is the content of my Article one.
+            </p>
             <p>
-        This is the content of my Article one.         This is the content of my Article one.        This is the content of my Article one
-                </p>;
-            <p>
-          This is the content of my Article one.        This is the content of my Article one.        This is the content of my Article one
+          This is the content of my Article one.        This is the content of my Article one.        This is the content of my Article one.
             </p>'
  
 };
